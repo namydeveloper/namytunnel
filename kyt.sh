255cfc5b78c0cbaad32ccc8265540bcc223e57df
namydeveloper <122620672+namydeveloper@users.noreply.github.com>
2024-01-06 16:33:26 +0700

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -22,7 +22,7 @@ mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/namydeveloper/namytunnel/main/limit/bot.zip
+wget https://raw.githubusercontent.com/namydeveloper/namytunnel/main/limit/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
