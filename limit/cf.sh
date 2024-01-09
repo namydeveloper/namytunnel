f16d321e04dfdb92d88beae1f517c6d10dacb930
namydeveloper <122620672+namydeveloper@users.noreply.github.com>
2024-01-10 01:49:59 +0700

    Update cf.sh

--- limit/cf.sh
+++ limit/cf.sh
@@ -23,15 +23,11 @@ exit 0
 fi
 clear
 apt install jq curl -y
-###DOM#AIN=installer.my.id
 DOMAIN=namystore.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c6)
 SUB_DOMAIN=${sub}.namystore.my.id
 CF_ID=namydevz@gmail.com
 CF_KEY=2ab3b1302dbd41f8ffa5f7564545b81e3c167
-##SUB_DOMAIN=${sub}.installer.my.id
-##CF_ID=senowahyu62@gmail.com
-##CF_KEY=84570683099becbeb0f04d8086ce6f309c56c
 set -euo pipefail
 IP=$(wget -qO- ipinfo.io/ip);
 echo "Updating DNS for ${SUB_DOMAIN}..."
