5cf9eea9329369c34b79ca953c0e8c0b4421c116
namydeveloper <122620672+namydeveloper@users.noreply.github.com>
2024-01-09 14:33:52 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -132,7 +132,7 @@ fi
 echo -e "\e[32mloading...\e[0m"
 clear
 # REPO    
-    REPO="https://raw.githubusercontent.com/namydeveloper/namytunnel/main/"
+    REPO="https://raw.githubusercontent.com/namydeveloper/Regist/main/"
 
 ####
 start=$(date +%s)
